c3b1c325137feb048f3f878a430e13c1e1af980e
Janak <janakrajbhatta@gmail.com>
2015-11-17 15:23:17 +0545

    show more show less directive implemented

---
+++ app.js
@@ -0,0 +1,73 @@
+angular.module('app', []);
+
+
+
+angular.module('app').controller('mainCtrl', ['$scope', function($scope) {
+
+    $scope.books = [{
+        name: 'PHP 7 For Beginner',
+        descripton: 'try. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem ',
+    }, {
+        name: 'Fundamentals of angularjs',
+        descripton: 'try. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem '
+    }, {
+        name: 'Universal Hacking',
+        descripton: 'try. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem '
+    }, {
+        name: 'PageMaker',
+        descripton: 'try. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem '
+    }];
+}])
+
+/**
+ *  Show more and show less in long text within ng-repeat.
+ *  eg. <show-more text="job.description"> </show-more>
+ */
+angular.module('app').directive('showMore', [function() {
+    return {
+        restrict: 'AE',
+        replace: true,
+        scope: {
+            text: '=',
+        },
+
+        template: '<div><p ng-show="largeText"> {{ text | subString :0 :end }}.... <a href="javascript:;" ng-click="showMore()" ng-show="isShowMore">Show More</a><a href="javascript:;" ng-click="showLess()" ng-hide="isShowMore">Show Less </a></p><p ng-hide="largeText">{{ text }}</p></div> ',
+
+        link: function(scope, iElement, iAttrs) {
+
+            var defaultEnd = 170;
+            scope.end = defaultEnd;
+            scope.isShowMore = true;
+            scope.largeText = true;
+
+            if (scope.text.length <= defaultEnd) {
+                scope.largeText = false;
+            };
+
+            scope.showMore = function() {
+
+                scope.end = scope.text.length;
+                scope.isShowMore = false;
+            };
+
+            scope.showLess = function() {
+
+                scope.end = defaultEnd;
+                scope.isShowMore = true;
+            };
+        }
+    };
+}]);
+
+
+/**
+ * Retrun the string from specified range.
+ * e.g<p><% course.description | subString : 0 :75 %> </p>
+ */
+angular.module('app').filter('subString', function() {
+    return function(str, start, end) {
+        if (str != undefined) {
+            return str.substr(start, end);
+        }
+    }
+})
